2d762b2486d45b404f7d723384b6710aff26b483
SKY <SKY@DESKTOP-33A8MLH>
2017-03-21 17:43:18 +0900

    dd

---
+++ src/co/kr/sky/hymnbible/LMSMyPhoneActivity.java
@@ -0,0 +1,13 @@
+package co.kr.sky.hymnbible;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LMSMyPhoneActivity extends Activity{
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_myphone);
+
+	}
+}
---
+++ src/co/kr/sky/hymnbible/LMSServerActivity.java
@@ -0,0 +1,13 @@
+package co.kr.sky.hymnbible;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LMSServerActivity extends Activity{
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_server);
+
+	}
+}
