e2107289d4ecfcf196acdc9f32d4eed165aed951
SKY <SKY@218.38.137.27>
2017-09-27 10:42:36 +0900

    ..

--- src/co/kr/sky/hymnbible/SubWebViewActivity.java
+++ src/co/kr/sky/hymnbible/SubWebViewActivity.java
@@ -2,6 +2,8 @@ package co.kr.sky.hymnbible;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+//아직 사용 안함..
 public class SubWebViewActivity extends Activity{
 
 	protected void onCreate(Bundle savedInstanceState) {
